66fda01f79d25fba2c8a4547d4c0201ed275c917
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-26 13:50:09 -0400

    Created the about us section, added sle and images

--- home/views.py
+++ home/views.py
@@ -14,7 +14,23 @@ class AboutPage(View):
     template_name = 'home/about.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        features = [
+            {'icon': 'M12 2L6 12h5v8h2v-8h5L12 2z', 'title': 'Velocidad garantizada',
+             'desc': 'Disfruta de la máxima velocidad sin interrupciones para todas tus actividades en línea.'},
+            {'icon': 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8h5z', 'title': 'Instalación rápida',
+             'desc': 'Nuestro equipo de profesionales te conectará en tiempo récord y sin complicaciones.'},
+            {
+                'icon': 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15H9v-2h2v2zm0-4H9v-2h2v2zm0-4H9V7h2v2zm4 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z',
+                'title': 'Planes flexibles',
+                'desc': 'Elige el plan que mejor se adapte a tus necesidades y presupuesto, sin contratos forzosos.'},
+            {'icon': 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 16h-2v-2h2v2zm0-4h-2V7h2v7z',
+             'title': 'Soporte técnico 24/7',
+             'desc': 'Nuestro equipo de soporte está siempre disponible para ayudarte, cualquier día y a cualquier hora.'}
+        ]
+        context = {'features': features}
+
+        return render(request, self.template_name, context)
+
 
 class ContactPage(View):
     template_name = 'home/contact.html'
