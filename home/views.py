fc1a3158677da95dd384b75217a0207a791fbea6
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-25 20:58:48 -0400

    Adding the plan sections with a comparison table, plus the plans and a top image.

--- home/views.py
+++ home/views.py
@@ -25,5 +25,18 @@ class ContactPage(View):
 class PlanPage(View):
     template_name = 'home/plan.html'
 
+
     def get(self, request):
-        return render(request, self.template_name)
+        plans = [
+            {'name': 'Básico', 'velocidad': '10 Mbps', 'soporte': '24/7', 'subida': '5 Mbs', 'latencia_baja': False},
+            {'name': 'Esencial', 'velocidad': '50 Mbps', 'soporte': '24/7', 'subida': '25 Mbs', 'latencia_baja': False},
+            {'name': 'Intermedio', 'velocidad': '80 Mbps', 'soporte': '24/7', 'subida': '40 Mbs',
+             'latencia_baja': True},
+            {'name': 'Avanzado', 'velocidad': '100 Mbps', 'soporte': '24/7', 'subida': '50 Mbs', 'latencia_baja': True},
+            {'name': 'Premium', 'velocidad': '150 Mbps', 'soporte': '24/7', 'subida': '75 Mbs', 'latencia_baja': True},
+            {'name': 'Ultra', 'velocidad': '200 Mbps', 'soporte': 'VIP 24/7', 'subida': '100 Mbs',
+             'latencia_baja': True},
+        ]
+
+        context = {'plans': plans}
+        return render(request, self.template_name, context)
