2bb7dbd7d64521761262b30ce15e0514ea21c92b
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-08-29 08:14:07 -0400

    adding new feature to the plan

--- home/views.py
+++ home/views.py
@@ -93,13 +93,13 @@ class PlanPage(View):
 
     def get(self, request):
         plans = [
-            {'name': 'Básico', 'velocidad': '10 Mbps', 'soporte': '24/7', 'subida': '5 Mbs', 'latencia_baja': False},
-            {'name': 'Esencial', 'velocidad': '50 Mbps', 'soporte': '24/7', 'subida': '25 Mbs', 'latencia_baja': False},
-            {'name': 'Intermedio', 'velocidad': '80 Mbps', 'soporte': '24/7', 'subida': '40 Mbs',
+            {'name': 'Básico', 'velocidad': '10 Mbps', 'soporte': '24/7', 'subida': '5 Mbs','Fibra_óptica': True, 'latencia_baja': False},
+            {'name': 'Esencial', 'velocidad': '50 Mbps', 'soporte': '24/7', 'subida': '25 Mbs', 'Fibra_óptica': True, 'latencia_baja': False},
+            {'name': 'Intermedio', 'velocidad': '80 Mbps', 'soporte': '24/7', 'subida': '40 Mbs', 'Fibra_óptica': True,
              'latencia_baja': True},
-            {'name': 'Avanzado', 'velocidad': '100 Mbps', 'soporte': '24/7', 'subida': '50 Mbs', 'latencia_baja': True},
-            {'name': 'Premium', 'velocidad': '150 Mbps', 'soporte': '24/7', 'subida': '75 Mbs', 'latencia_baja': True},
-            {'name': 'Ultra', 'velocidad': '200 Mbps', 'soporte': 'VIP 24/7', 'subida': '100 Mbs',
+            {'name': 'Avanzado', 'velocidad': '100 Mbps', 'soporte': '24/7', 'subida': '50 Mbs', 'Fibra_óptica': True, 'latencia_baja': True},
+            {'name': 'Premium', 'velocidad': '150 Mbps', 'soporte': '24/7', 'subida': '75 Mbs', 'Fibra_óptica': True, 'latencia_baja': True},
+            {'name': 'Ultra', 'velocidad': '200 Mbps', 'soporte': 'VIP 24/7', 'subida': '100 Mbs', 'Fibra_óptica': True,
              'latencia_baja': True},
         ]
 
